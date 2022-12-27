c688b724d1a24682d33995e7c9acdc998aa59d3f
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2022-12-27 16:06:02 +0800

    Add multiple select todos

--- spacialMemoryTodo/Workspace/CardView/LocationCardOutlineView.swift
+++ spacialMemoryTodo/Workspace/CardView/LocationCardOutlineView.swift
@@ -19,6 +19,7 @@ class LocationCardOutlineView: macAppBoilerplate.OutlineViewController {
         outlineView.dataSource = self
         outlineView.menu = NSMenu()
         outlineView.menu?.delegate = self
+        outlineView.allowsMultipleSelection = true
     }
 }
 
@@ -50,27 +51,62 @@ extension LocationCardOutlineView: NSOutlineViewDataSource, NSOutlineViewDelegat
 
 extension LocationCardOutlineView: NSMenuDelegate {
     func menuNeedsUpdate(_ menu: NSMenu) {
-        let row = outlineView.clickedRow
-        guard row >= 0, let item = outlineView.item(atRow: row) as? Todo else {
-            menu.items = []
+        menu.items = []
+
+        // check for single-row or no-row selection clicks
+        let rows = outlineView.selectedRowIndexes
+        if rows.count <= 1 {
+            let row = outlineView.clickedRow
+            guard row >= 0, let item = outlineView.item(atRow: row) as? Todo else {
+                return
+            }
+
+            menu.items = [
+                .init(title: "Mark As \(item.isDone ? "Not " : "")Done",
+                      action: #selector(toggleTodoDone),
+                      keyEquivalent: ""),
+                .init(title: "Edit Todo",
+                      action: #selector(editTodo),
+                      keyEquivalent: ""),
+                .init(title: "Delete Todo",
+                      action: #selector(deleteTodo),
+                      keyEquivalent: "")
+            ]
+
             return
         }
 
-        menu.items = [
-            .init(title: "Mark As \(item.isDone ? "Not " : "")Done",
-                  action: #selector(markTodoAsDone),
-                  keyEquivalent: ""),
-            .init(title: "Edit Todo",
-                  action: #selector(editTodo),
-                  keyEquivalent: ""),
-            .init(title: "Delete Todo",
-                  action: #selector(deleteTodo),
-                  keyEquivalent: "")
-        ]
+        var items: [Todo] = []
+        for row in rows {
+            guard row >= 0,
+                  let item = outlineView.item(atRow: row) as? Todo else {
+                continue
+            }
+            items.append(item)
+        }
+
+        let doneCount = items.filter({ $0.isDone }).count
+        let notDoneCount = items.count - doneCount
+
+        if notDoneCount > 0 {
+            menu.items.append(.init(title: "Mark \(notDoneCount) Todos As Done",
+                                    action: #selector(markTodosAsDone),
+                                    keyEquivalent: ""))
+        }
+
+        if doneCount > 0 {
+            menu.items.append(.init(title: "Mark \(doneCount) Todos As Not Done",
+                                    action: #selector(markTodosAsNotDone),
+                                    keyEquivalent: ""))
+        }
+
+        menu.items.append(.init(title: "Delete \(items.count) Todos",
+                                action: #selector(deleteTodo),
+                                keyEquivalent: ""))
     }
 
     @objc
-    func markTodoAsDone() {
+    func toggleTodoDone() {
         guard let item = clickedTodo() else { return }
 
         location.toggleTodoDone(withID: item.id)
@@ -88,9 +124,33 @@ extension LocationCardOutlineView: NSMenuDelegate {
 
     @objc
     func deleteTodo() {
-        guard let item = clickedTodo() else { return }
+        let items = selectedTodos()
 
-        location.removeTodo(withID: item.id)
+        for item in items {
+            location.removeTodo(withID: item.id)
+        }
+        LocationManager.save(sender: self.view)
+    }
+
+    @objc
+    func markTodosAsDone() {
+        let items = selectedTodos()
+
+        for item in items {
+            item.isDone = true
+        }
+        location.objectWillChange.send()
+        LocationManager.save(sender: self.view)
+    }
+
+    @objc
+    func markTodosAsNotDone() {
+        let items = selectedTodos()
+
+        for item in items {
+            item.isDone = false
+        }
+        location.objectWillChange.send()
         LocationManager.save(sender: self.view)
     }
 
@@ -98,5 +158,13 @@ extension LocationCardOutlineView: NSMenuDelegate {
         let row = outlineView.clickedRow
         guard row >= 0 else { return nil }
         return outlineView.item(atRow: row) as? Todo
+    }
+
+    func selectedTodos() -> [Todo] {
+        let rows = outlineView.selectedRowIndexes
+
+        return rows.compactMap { row in
+            outlineView.item(atRow: row) as? Todo
+        }
     }
 }

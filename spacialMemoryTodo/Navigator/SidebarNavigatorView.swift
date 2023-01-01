de519eb25b975ab6601fd119a2020ca9f6c1f342
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-01-01 21:37:49 +0800

    Separate toolbar into its own file

--- spacialMemoryTodo/Navigator/SidebarNavigatorView.swift
+++ spacialMemoryTodo/Navigator/SidebarNavigatorView.swift
@@ -9,19 +9,11 @@ import SwiftUI
 import macAppBoilerplate
 
 struct SidebarNavigatorView: View {
-    @Environment(\.colorScheme) var colorScheme
-
     @EnvironmentObject
     var tabManager: TabManager
 
     @ObservedObject
     var popUpManager: PopUpManager
-
-    @State
-    var hideCompletedTodos: Bool = false
-
-    @State
-    var searchTerm: String = ""
 
     var body: some View {
         VStack {
@@ -32,67 +24,7 @@ struct SidebarNavigatorView: View {
             }
         }
         .safeAreaInset(edge: .bottom) {
-            VStack {
-                HStack {
-                    HStack {
-                        sortButton
-                        TextField("Filter", text: $searchTerm)
-                            .textFieldStyle(.plain)
-                            .font(.system(size: 12))
-                        if !searchTerm.isEmpty {
-                            Button {
-                                searchTerm = ""
-                            } label: {
-                                Image(systemName: "xmark")
-                            }
-                            .font(.system(size: 12))
-                            .buttonStyle(.borderless)
-                            .padding(.trailing, 3)
-                        }
-                    }
-                    .padding(.vertical, 3)
-                    .background(colorScheme == .dark ? Color(hex: "#FFFFFF").opacity(0.1) :
-                                    Color(hex: "#808080").opacity(0.2))
-                    .clipShape(RoundedRectangle(cornerRadius: 6))
-                    .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 0.5).cornerRadius(6))
-                    .padding(.trailing, 3)
-                    .padding(.leading, 5)
-                }
-                .frame(height: 29, alignment: .center)
-                .frame(maxWidth: .infinity)
-                .overlay(alignment: .top) {
-                    Divider()
-                }
-            }
-            .padding(.top, -8)
-        }
-    }
-
-    var sortButton: some View {
-        Menu {
-            Button {
-                hideCompletedTodos.toggle()
-            } label: {
-                Text("\(hideCompletedTodos ? "Show" : "Hide") Completed Todos")
-            }
-        } label: {
-            Image(systemName: "line.3.horizontal.decrease.circle")
-        }
-        .menuStyle(.borderlessButton)
-        .frame(maxWidth: 30)
-        .opacity(1)
-        .onAppear {
-            let locationManager = LocationManager.default
-            hideCompletedTodos = locationManager.hideCompletedTodos
-            searchTerm = locationManager.searchTerm
-        }
-        .onChange(of: hideCompletedTodos) { newValue in
-            let locationManager = LocationManager.default
-            locationManager.hideCompletedTodos = newValue
-        }
-        .onChange(of: searchTerm) { newValue in
-            let locationManager = LocationManager.default
-            locationManager.searchTerm = newValue
+            SidebarNavigatorViewToolbar()
         }
     }
 }
---
+++ spacialMemoryTodo/Navigator/SidebarNavigatorViewToolbar.swift
@@ -0,0 +1,83 @@
+//
+//  SidebarNavigatorViewToolbar.swift
+//  spacialMemoryTodo
+//
+//  Created by Kai Quan Tay on 1/1/23.
+//
+
+import SwiftUI
+
+struct SidebarNavigatorViewToolbar: View {
+
+    @Environment(\.colorScheme) var colorScheme
+
+    @State
+    var hideCompletedTodos: Bool = false
+
+    @State
+    var searchTerm: String = ""
+
+    var body: some View {
+        VStack {
+            HStack {
+                HStack {
+                    sortButton
+                    TextField("Filter", text: $searchTerm)
+                        .textFieldStyle(.plain)
+                        .font(.system(size: 12))
+                    if !searchTerm.isEmpty {
+                        Button {
+                            searchTerm = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                        }
+                        .font(.system(size: 12))
+                        .buttonStyle(.borderless)
+                        .padding(.trailing, 3)
+                    }
+                }
+                .padding(.vertical, 3)
+                .background(colorScheme == .dark ? Color(hex: "#FFFFFF").opacity(0.1) :
+                                Color(hex: "#808080").opacity(0.2))
+                .clipShape(RoundedRectangle(cornerRadius: 6))
+                .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 0.5).cornerRadius(6))
+                .padding(.trailing, 3)
+                .padding(.leading, 5)
+            }
+            .frame(height: 29, alignment: .center)
+            .frame(maxWidth: .infinity)
+            .overlay(alignment: .top) {
+                Divider()
+            }
+        }
+        .padding(.top, -8)
+    }
+
+    var sortButton: some View {
+        Menu {
+            Button {
+                hideCompletedTodos.toggle()
+            } label: {
+                Text("\(hideCompletedTodos ? "Show" : "Hide") Completed Todos")
+            }
+        } label: {
+            Image(systemName: "line.3.horizontal.decrease.circle")
+        }
+        .menuStyle(.borderlessButton)
+        .frame(maxWidth: 30)
+        .opacity(1)
+        .onAppear {
+            let locationManager = LocationManager.default
+            hideCompletedTodos = locationManager.hideCompletedTodos
+            searchTerm = locationManager.searchTerm
+        }
+        .onChange(of: hideCompletedTodos) { newValue in
+            let locationManager = LocationManager.default
+            locationManager.hideCompletedTodos = newValue
+        }
+        .onChange(of: searchTerm) { newValue in
+            let locationManager = LocationManager.default
+            locationManager.searchTerm = newValue
+        }
+    }
+}
